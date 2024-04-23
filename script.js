238b1937b031f88ce7f9229a071675d55a8af343
Gnana Prakash M <gnanaprakash2918@gmail.com>
2024-04-23 22:32:30 +0530

    Add Check Winner Logic

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const GameBoard = (() => {
-  const square = {
-    mark: '',
-  };
-
   const board = [];
+
   const getBoard = () => {
     return board;
   };
 
-  const newMarker = (mark, index) => {
-    board[index] = { mark };
+  const newMarker = (mark, row, col) => {
+    board[row][col] = { mark };
   };
 
   const init = () => {
-    for (let count = 1; count <= 9; ++count) {
-      board.push(square);
+    for (let i = 1; i <= 3; ++i) {
+      const row = [];
+      for (let j = 1; j <= 3; ++j) {
+        row.push({ mark: '' });
+      }
+
+      board.push(row);
     }
   };
 
@@ -25,17 +27,57 @@ const GameBoard = (() => {
   };
 })();
 
+const checkWinner = () => {
+  const board = GameBoard.getBoard();
+
+  // Check Rows
+  for (let i = 0; i < 3; ++i) {
+    const row = board[i];
+    if (row.every((val) => val == row[0])) {
+      if (row[0].mark !== '') return row[0].mark;
+    }
+  }
+
+  // Check Cols
+  for (let j = 0; j < 3; ++j) {
+    const col = [];
+    for (let i = 0; i < 3; ++i) {
+      col.push(board[i][j]);
+    }
+
+    if (col.every((val) => val == col[0])) {
+      if (col[0].mark !== '') return col[0].mark;
+    }
+  }
+
+  // Check If Filled
+  let checkEmpty = false;
+  for (let i = 0; i < 3; ++i) {
+    for (let j = 0; j < 3; ++j) {
+      if (board[i][j].mark === '') {
+        checkEmpty = true;
+        break;
+      }
+    }
+  }
+
+  // Tie
+  if (!checkEmpty) {
+    return 'T';
+  }
+
+  return '';
+};
+
 const Controller = (() => {
   const playerOne = {
     name: 'Player 1',
     marker: 'X',
-    type: '',
   };
 
   const playerTwo = {
     name: 'Player 2',
     marker: 'O',
-    type: '',
   };
 
   let playerOneTurn = true;
